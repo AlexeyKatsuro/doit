f5fbd922586a5068ff0a40d9450d5aad5d66e2f2
Alexey Katsuro <katsuro.alexey@gmail.com>
2023-01-29 13:59:41 +0100

    feat(ui_mocks): add Sign Up page at mocks

---
+++ packages/ui_mocks/lib/features/auth/sign_up/fixtures.dart
@@ -0,0 +1,7 @@
+import 'package:doit_ui_mocks/features/auth/sign_up/sign_up_view_model.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ui/ui.dart';
+
+final signUpPages = <String, WidgetBuilder>{
+  '/sign-up': (_) => const SignUpPage(viewModel: SignUpViewModelMock()),
+};
--- packages/ui_mocks/lib/features/routes.dart
+++ packages/ui_mocks/lib/features/routes.dart
@@ -1,10 +1,12 @@
 
 
 import 'package:doit_ui_mocks/features/auth/sign_in/fixtures.dart';
+import 'package:doit_ui_mocks/features/auth/sign_up/fixtures.dart';
 import 'package:doit_ui_mocks/features/home/fixtures.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   ...signInPages,
+  ...signUpPages,
   ...homePages,
 };
